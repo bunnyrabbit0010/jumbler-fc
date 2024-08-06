3f910f8a03ef1c573d53b953ac19cb3dac60a40d
“Bunny <BunnyRabbit_aws@gmail.com>
2024-08-06 16:31:25 -0700

    working version!!!

--- jmb-front-end-1/src/App.js
+++ jmb-front-end-1/src/App.js
@@ -1,12 +1,147 @@
+
+import React, { useEffect } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 import getWords from './getWords';
 
 
+  
+
 function App() {
-  console.debug('Invoking getWords()')
-  const wordMap = getWords();
+
+  const addWords = () => {
+    console.log('Invoking getWords()')
+    //const wordMap =  getWords()
+    //let jsonArray =[];
+    getWords().then(response => {
+      // Do something with the response
+      console.log('*!*!');
+      console.log(response);
+      console.log(typeof(response));
+      return JSON.parse(response);
+      //return response.json(); // Return the parsed JSON data
+    })
+    .then(jsonArray => {
+      if (jsonArray) {
+        const container = document.getElementById('container');
+        const ul = document.createElement('ul');
+    
+        jsonArray.forEach(item => {
+          const li = document.createElement('li');
+          li.textContent = `${item.Word} - ${item.jumbled_word}`;
+          console.log(li.textContent);
+          ul.appendChild(li);
+        });
+    
+        container.appendChild(ul);
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });  
+  };
+
+  useEffect(() => {
+    addWords();
+  }, []);
+
+      return (
+        <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+          </header>
+          <p></p>
  
+          <div id="container">
+           </div>
+         </div>
+      );
+    }
+  export default App;
+
+/*
+function addWords() {
+  console.log('Invoking getWords()')
+  //const wordMap =  getWords();
+
+  getWords().then(response => {
+    // Do something with the response
+    console.log('*!*!');
+    console.log(response);
+    console.log(typeof(response));
+  
+    return response.json(); // Return the parsed JSON data
+  })
+  .then(jsonArray => {
+    if (jsonArray) {
+      const container = document.getElementById('container');
+      const ul = document.createElement('ul');
+  
+      jsonArray.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.Word} - ${item.jumbled_word}`;
+        console.log(li.textContent);
+        ul.appendChild(li);
+      });
+  
+      container.appendChild(ul);
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+ 
+}
+*/
+
+/*
+  getWords().then(response => {
+    // Do something with the response
+    console.log('*!*!')
+    console.log(response);
+    console.log(typeof(response)));
+    .then (
+    const jsonArray = JSON.parse(response);
+    if(jsonArray) {
+      const container = document.getElementById('container');
+
+      // Create a new paragraph element
+      const paragraph = document.createElement('p');
+      paragraph.textContent = 'This is a dynamically added paragraph.';
+
+      const ul = document.createElement('ul');
+      for (let i = 0; i < jsonArray.length; i++) {
+        console.log(jsonArray[i].Word, jsonArray[i].jumbled_word);
+        const li = document.createElement('li');
+        li.textContent = `${item.Word} - ${item.jumbled_word}`;
+        ul.appendChild(li);      
+      }
+      container.appendChild(ul);
+    }
+  }
+}
+  */
+
+
+
+ /*
+ 
+
+  <!--<ul>
+            {
+              response.forEach(item => {
+                console.log(item.word, item.jumbled_word);
+                <li>item.Word</li>
+              })
+            }
+          </ul> 
+
+                   <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            </header>
+
+
   if (wordMap) { // Check if wordList is not undefined
     console.log("Logging response after getWords()" + wordMap);
     console.debug(wordMap);
@@ -32,6 +167,4 @@ function App() {
     // Handle the case where getWords() returns undefined (e.g., display error message)
     return <div>Error fetching data</div>;
   }
-}
-
-export default App;
+    return <div>No words found</div>;*/
--- jmb-front-end-1/src/getWords.js
+++ jmb-front-end-1/src/getWords.js
@@ -2,12 +2,11 @@ const getWords = async () => {
     console.log('In getWords');
 
     const apiUrl = 'https://ee98qt6an2.execute-api.us-east-2.amazonaws.com/test';
-    const apiKey = 'AHq559oDgvajhtXir2lYU4R8oDb57tlB7oP8EZUV'
+    //const apiKey = 'AHq559oDgvajhtXir2lYU4R8oDb57tlB7oP8EZUV'
 
-    const wordMap = new Map();
     let jsonData = {};
 
-    const apiResponse = await fetch(apiUrl, {
+    await fetch(apiUrl, {
         headers: {
           'Content-Type': 'Application/json',
           'Authorization':  'Bearer ${apiKey}'
@@ -17,9 +16,9 @@ const getWords = async () => {
     .then ((data) => {
       if (data.statusCode === 200) {
         jsonData = data.body;
-        console.log("****");
-        console.log(jsonData);
-        console.log("****");   
+       // console.log("****");
+        //console.log(jsonData);
+       // console.log("****");   
       }
       else {
         console.log("API call failed");
@@ -28,17 +27,6 @@ const getWords = async () => {
     .catch(console.error);
   
 
-    /*if(wordMap.size > 0) {
-      //console.log ('Returning from getWords ' + wordMap.keys + ' ' + wordMap.values);
-      wordMap.forEach((value, key) => {
-        console.log(`Key: ${key}, Value: ${value}`);
-      });    
-    }
-    else {
-      console.log ('Returning empty list');
-    }
-
-    return wordMap;*/
     console.log ('Returning: ' + jsonData);
     return jsonData;
 }
